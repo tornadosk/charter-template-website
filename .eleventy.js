0800ff29197d8c2dd6d9855455375db07aaff84f
tornadosk <60984219+tornadosk@users.noreply.github.com>
2025-06-20 02:09:45 -0400

    design new tests fixes

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("admin");
+
+  // Dynamic event loader for folder-based JSON
+  eleventyConfig.addCollection("events", function () {
+    const eventFiles = fs.readdirSync("./content/events");
+    return eventFiles.map(file => {
+      const data = JSON.parse(fs.readFileSync(`./content/events/${file}`));
+      return data;
+    });
+  });
+
   return {
     dir: {
       input: ".",
