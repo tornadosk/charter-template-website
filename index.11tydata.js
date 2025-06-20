ad82aef2b9ea32ce764e1d78b6e75dc2b9080c49
tornadosk <60984219+tornadosk@users.noreply.github.com>
2025-06-20 01:27:20 -0400

    design new

--- index.11tydata.js
+++
@@ -1,8 +0,0 @@
-const chapter = require("./content/chapter.json");
-const events = require("./content/events.json");
-
-module.exports = {
-  chapter,
-  events,
-  currentYear: new Date().getFullYear()
-};
